08a17ae5eb78d9eabdf83f3d9dee00597b0942fd
YaShock <jashok12@gmail.com>
2018-07-07 13:32:11 +0200

    Proper formatting

--- main.cpp
+++ main.cpp
@@ -6,11 +6,9 @@ int main()
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
