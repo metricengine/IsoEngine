0c32e2bda8680484bf9647453c54ab6f2d0ec701
marton bognar <martonbognar@sch.bme.hu>
2018-07-18 23:02:10 +0200

    Dummy main function with a texture, sprite, mouse movement and resize handling

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,32 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "IsoEngine");
+
+    sf::Texture texture;
+    texture.loadFromFile("../res/textures/dummy.png");
+    texture.setSmooth(true);
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setOrigin(texture.getSize().x / 2, texture.getSize().y / 2);
 
     while (window.isOpen()) {
-        sf::Event event;
+        sf::Event event{};
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+
+            if (event.type == sf::Event::Resized) {
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
+            }
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::White);
+
+        sprite.setPosition(sf::Vector2f(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y));
+        window.draw(sprite);
         window.display();
     }
 
